13cd0a374ed16967af88464b76f18554beb39c0c
Karolina Machniewska <karolina.machniewska@gmail.com>
2019-04-08 19:06:17 +0200

    new quotes added

--- js/main.js
+++ js/main.js
@@ -10,12 +10,16 @@
                     "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends.",
                     "Fear of a name only increases fear of the thing itself.",
                     "But you know, happiness can be found even in the darkest of times, if one only remembers to turn on the light.",
+                    "It matters not what someone is born, but what they grow to be.",
+                    "Things we lose have a way of coming back to us in the end, if not always in the way we expect. ",
+                    
+
 
 
     ];
     
     // RANDOM CHOSE FROM ALL QUOTES
-    let randomQuote = Math.floor(Math.random() * 4);
+    let randomQuote = Math.floor(Math.random() * 6);
     
     
     function showText(e) {
